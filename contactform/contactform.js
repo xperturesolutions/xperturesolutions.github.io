ac425bf73c02eab3e4ea5c16f55c54dfc78ef4e6
Loveneet Singh Chahal <chahal.loveneet@gmail.com>
2025-01-15 11:11:46 +0530

    updated changes

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -1,118 +1,112 @@
-jQuery(document).ready(function($) {
-  "use strict";
+"use strict";
 
-  //Contact
-  $('form.contactForm').submit(function() {
-    var f = $(this).find('.form-group'),
+jQuery(document).ready(function ($) { 
+  // Contact form submission
+  $("form.contactForm").submit(function () {
+    var f = $(this).find(".form-group"),
       ferror = false,
-      emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+      emailExp = /^[^\s()<>@,;:/]+@\w[\w.-]+\.[a-z]{2,}$/i;
 
-    f.children('input').each(function() { // run all inputs
-
-      var i = $(this); // current input
-      var rule = i.attr('data-rule');
-
+    // Validate inputs
+    f.children("input").each(function () {
+      var i = $(this); // Current input
+      var rule = i.attr("data-rule");
       if (rule !== undefined) {
-        var ierror = false; // error flag for current input
-        var pos = rule.indexOf(':', 0);
-        if (pos >= 0) {
-          var exp = rule.substr(pos + 1, rule.length);
-          rule = rule.substr(0, pos);
-        } else {
-          rule = rule.substr(pos + 1, rule.length);
-        }
+        var ierror = false; // Error flag for current input
+        var pos = rule.indexOf(":");
+        var exp = pos >= 0 ? rule.slice(pos + 1) : null;
+        rule = pos >= 0 ? rule.slice(0, pos) : rule;
 
         switch (rule) {
-          case 'required':
-            if (i.val() === '') {
+          case "required":
+            if (i.val() === "") {
               ferror = ierror = true;
             }
             break;
-
-          case 'minlen':
+          case "minlen":
             if (i.val().length < parseInt(exp)) {
               ferror = ierror = true;
             }
             break;
-
-          case 'email':
+          case "email":
             if (!emailExp.test(i.val())) {
               ferror = ierror = true;
             }
             break;
-
-          case 'checked':
-            if (! i.is(':checked')) {
+          case "checked":
+            if (!i.is(":checked")) {
               ferror = ierror = true;
             }
             break;
-
-          case 'regexp':
+          case "regexp":
             exp = new RegExp(exp);
             if (!exp.test(i.val())) {
               ferror = ierror = true;
             }
             break;
+          default:
+            console.warn(`Unknown rule: ${rule}`);
         }
-        i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+        i.next(".validation")
+          .html(ierror ? i.attr("data-msg") || "Wrong Input" : "")
+          .show("blind");
       }
     });
-    f.children('textarea').each(function() { // run all inputs
 
-      var i = $(this); // current input
-      var rule = i.attr('data-rule');
-
+    // Validate textareas
+    f.children("textarea").each(function () {
+      var i = $(this);
+      var rule = i.attr("data-rule");
       if (rule !== undefined) {
-        var ierror = false; // error flag for current input
-        var pos = rule.indexOf(':', 0);
-        if (pos >= 0) {
-          var exp = rule.substr(pos + 1, rule.length);
-          rule = rule.substr(0, pos);
-        } else {
-          rule = rule.substr(pos + 1, rule.length);
-        }
+        var ierror = false;
+        var pos = rule.indexOf(":");
+        var exp = pos >= 0 ? rule.slice(pos + 1) : null;
+        rule = pos >= 0 ? rule.slice(0, pos) : rule;
 
         switch (rule) {
-          case 'required':
-            if (i.val() === '') {
+          case "required":
+            if (i.val() === "") {
               ferror = ierror = true;
             }
             break;
-
-          case 'minlen':
+          case "minlen":
             if (i.val().length < parseInt(exp)) {
               ferror = ierror = true;
             }
             break;
+          default:
+            console.warn(`Unknown rule: ${rule}`);
         }
-        i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+        i.next(".validation")
+          .html(ierror ? i.attr("data-msg") || "Wrong Input" : "")
+          .show("blind");
       }
     });
+
+    // If errors exist, stop form submission
     if (ferror) return false;
-    else var str = $(this).serialize();
-    var action = $(this).attr('action');
-    if( ! action ) {
-      action = 'contactform.php';
-    }
+
+    // Prepare form data
+    var str = $(this).serialize();
+    console.log('action is'+$(this).attr("action"));
+    var action = $(this).attr("action") || "contactform.php"; 
+    // AJAX form submission
     $.ajax({
       type: "POST",
       url: action,
       data: str,
-      success: function(msg) {
-        // alert(msg);
-        if (msg == 'OK') {
-          $("#sendmessage").addClass("show");
-          $("#errormessage").removeClass("show");
-          $('.contactForm').find("input, textarea").val("");
-        } else {
-          $("#sendmessage").removeClass("show");
-          $("#errormessage").addClass("show");
-          $('#errormessage').html(msg);
-        }
-
-      }
+      success: function (msg) {  
+        if (msg === "Your message has been sent successfully.") {
+            $("#sendmessage").addClass("show");
+            $("#errormessage").removeClass("show");
+            $('.contactForm').find("input, textarea").val("");
+          } else {
+            $("#sendmessage").removeClass("show");
+            $("#errormessage").addClass("show").html(msg);
+          }
+      },
     });
+
     return false;
   });
-
 });
